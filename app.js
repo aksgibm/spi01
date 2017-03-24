11a2a6f7d5abfac5ec743496bf014a385ec1b3fd
aksgibm <a1kosugi@jp.ibm.com>
2017-03-24 20:51:09 +0900

    initial project setting both for local and bluemix.

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+//var cfenv = require('cfenv');
+//cfenv always makes trouble...
+
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+//The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
 
 // create a new express server
 var app = express();
@@ -18,11 +26,8 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(port, host, function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  console.log("server starting on " + host + ":" + port);
 });
