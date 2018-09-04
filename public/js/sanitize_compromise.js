09d5529167fbf7b2c0e78c2607365a8260a60af4
Akihiro Kosugi <a1kosugi@jp.ibm.com>
2018-09-04 16:24:10 +0900

    revisit filter tags and add us cities.

--- public/js/sanitize_compromise.js
+++ public/js/sanitize_compromise.js
@@ -1,7 +1,24 @@
 (($)=>{
 
-function _needs_filtered(tags){
-	return tags.Noun && (tags.ProperNoun || tags.Place || tags.Region || tags.FirstName || tags.LastName || tags.Organization || tags.Acronym || tags.Sensitive);
+function _validate_with_filters(tags, filters){
+	return filters.some((elm)=>{
+		return tags[elm];
+	});
+}	
+
+const _tags_tb_filtered = [
+//	"ProperNoun",
+	"Place",
+	"Region",
+	"FirstName",
+	"LastName",
+	"Organization",
+	"Acronym",
+	"PhoneNumber",
+	"Sensitive"
+];
+function _needs_filtered(tags, exttags){
+	return _validate_with_filters(tags, _tags_tb_filtered.concat(exttags || []));
 }
 
 window["sanitize_text_compromise"] = (option)=>{
@@ -18,24 +35,26 @@ window["sanitize_text_compromise"] = (option)=>{
 		return itxt;
 	}
 	return new Promise((resolve,reject)=>{
-		const doc = nlp(text);
-		var result = doc.list.map((stc)=>{
-			return stc.terms.map((trm)=>{
-				return [trm.whitespace.before,
-					_needs_filtered(trm.tags) ? trm.text.replace(/./g, outchr) : trm.text,
-					trm.whitespace.after
-				].join("");
-			}).join("");
-		}).join("");
-		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+/g);
-		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+/g);
-		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+/g);
-		result = _sanitize_reg(result, /[\w_-]+@[\w\.-]+\.\w{2,}/g);
+		var result = text;
 		if(opt.extfilters){
 			opt.extfilters.forEach(function(fltr){
 				result = _sanitize_reg(result,fltr);
 			});
 		}
+		const doc = nlp(result);
+		result = doc.list.map((stc)=>{
+			return stc.terms.map((trm)=>{
+				return [trm.whitespace.before,
+					_needs_filtered(trm.tags, opt.tagsfiltered) ? trm.text.replace(/./g, outchr) : trm.text,
+					trm.whitespace.after
+				].join("");
+			}).join("");
+		}).join("");
+//		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+/g);
+//		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+[-ー\/](\d|[０-９])+/g);
+//		result = _sanitize_reg(result, /(\d|[０-９])+[-ー\/](\d|[０-９])+/g);
+		result = _sanitize_reg(result, /[\w_-]+@[\w\.-]+\.\w{2,}/g);
+
 		resolve({"result": result});
 	});
 };
